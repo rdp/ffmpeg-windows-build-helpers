1599491614002069b1d20ea53455bf6604e7ad0e
rogerdpack <rogerpack2005@gmail.com>
2012-07-30 14:35:48 -0600

    notes

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 read -p '##################### Welcome ######################
 First we will download and compile a gcc cross-compiler (MinGW-w64).
-You will be prompted with a few questions as it installs.
+You will be prompted with a few questions as it installs (it takes quite awhile).
 Enter to continue:'
 
 wget http://zeranoe.com/scripts/mingw_w64_build/mingw-w64-build-3.0.6
