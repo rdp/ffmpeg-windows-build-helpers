e0288ca5d8132a4e305a76b4c47de9bf4629c76c
rogerdpack <rogerpack2005@gmail.com>
2012-08-04 20:54:39 -0600

    notez

--- many.rb
+++ many.rb
@@ -2,7 +2,9 @@ dec26 = "C:\\downloads\\ffmpeg-git-f514695-win32-static\\ffmpeg-git-f514695-win3
 dec22 =  "C:\\downloads\\ffmpeg-git-dd1fb65-win32-static\\ffmpeg-git-dd1fb65-win32-static"
 seven26 = "c:\\downloads\\ffmpeg-20120726-git-236ecc3-win32-shared\\ffmpeg-20120726-git-236ecc3-win32-shared"
 jan5 = "C:\\downloads\\ffmpeg-git-7f83db3-win32-static\\ffmpeg-git-7f83db3-win32-static"
-c = "#{jan5}\\bin\\ffmpeg.exe -threads 6 -y -i sintel.mpg -pass 1 -t 75 -c:v libx264 -an nul.mp4"
+aug4 = "C:\\downloads\\ffmpeg-20120804-git-f857465-win32-static\\ffmpeg-20120804-git-f857465-win32-static"
+
+c = "#{aug4}\\bin\\ffmpeg.exe -threads 6 -y -i sintel.mpg -pass 1 -t 75 -c:v libx264 -an nul.mp4"
 
 times = []
 loop { 
@@ -10,6 +12,6 @@ start = Time.now
 system(c)
 elapsed = Time.now - start
 times <<elapsed
-p times.sort, times.length, elapsed
+p times.sort, times.length, elapsed, c
 #sleep 1
 }
