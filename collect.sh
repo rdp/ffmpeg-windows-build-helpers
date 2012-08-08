60897fb719afc142be5ddd6f185b58c80f5c4b53
rogerdpack <rogerpack2005@gmail.com>
2012-08-08 13:58:37 -0600

    use date first

--- collect.sh
+++ collect.sh
@@ -3,7 +3,8 @@ git_version=`git rev-parse HEAD`
 echo $git_version
 cd ../../..
 mkdir -p distros
-dir="distros/ffmpeg-distro-static-$git_version"
+date=`date +%Y-%m-%d`
+dir="distros/ffmpeg-distro-static-$date-$git_version"
 mkdir "$dir"
 cp ./builds/win32/ffmpeg_git/ffmpeg.exe "$dir/ffmpeg-32.exe"
 cp ./builds/x86_64/ffmpeg_git/ffmpeg.exe "$dir/ffmpeg-x86_64.exe"
