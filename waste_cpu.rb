00237128c28f7dac50c669735b1a696c35eb9805
rogerdpack <rogerpack2005@gmail.com>
2012-08-15 10:52:38 -0600

    tweak script

--- waste_cpu.rb
+++ waste_cpu.rb
@@ -1,2 +1,5 @@
-10.times { Thread.new { loop {} } }
+raise 'need jruby since it uses threads currently' unless RUBY_DESCRIPTION =~ /jruby/
+threads = 5
+puts "ctrl+c to stop, using #{threads} threads"
+threads.times { Thread.new { loop {} } }
 sleep
