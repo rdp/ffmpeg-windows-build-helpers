99786e37e1c174531ee0dc255c4bfdff5adefa1f
rogerdpack <rogerpack2005@gmail.com>
2012-08-20 11:03:35 -0600

    remove file

--- repro.sh
+++
@@ -1,4 +0,0 @@
-  mkdir temp
-  wget http://zeranoe.com/scripts/mingw_w64_build/mingw-w64-build-3.0.6 -O mingw-w64-build-3.0.6
-  chmod u+x mingw-w64-build-3.0.6
-  ./mingw-w64-build-3.0.6 --mingw-w64-ver=2.0.4 --clean-build
