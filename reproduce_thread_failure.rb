384b7f8b86e21191ea3197e8786b0ffe27da6cb9
rogerdpack <rogerpack2005@gmail.com>
2012-09-25 09:00:08 -0600

    oddly varying timings...might not be related to the threading model used :)

--- reproduce_thread_failure.rb
+++ reproduce_thread_failure.rb
@@ -8,7 +8,11 @@ aug16 = "C:\\downloads\\ffmpeg-20120816-git-f0896a6-win32-static\\ffmpeg-2012081
 
 command = "#{aug16}\\bin\\ffmpeg.exe"
 #command = "ffmpeg_me_x86_64.exe"
-command= ".\\ffmpeg_win32_pthreads_x264_win32_pthreads.exe"
+#command= ".\\ffmpeg_win32_pthreads_x264_win32_pthreads.exe"
+command = "ffmpeg_win32_pthreads.exe"
+command = "C:\\downloads\\ffmpeg-distro-static-2012-09-12-6d743136a1c0e36416f598b43e68e2486ff4f214.libopus\\ffmpeg-distro-static-2012-09-12-6d743136a1c0e36416f598b43e68e2486ff4f214\\ffmpeg-32.exe"
+command = "C:\\downloads\\ffmpeg-distro-static-2012-08-24-5ac603df83e9f25d5ad5153b101e065783dfdb96.dshow_pixfmt.ramiro\\ffmpeg-distro-static-2012-08-24-5ac603df83e9f25d5ad5153b101e065783dfdb96\\ffmpeg-32.exe"
+
 threads=6
 c = "#{command} -threads #{threads} -y -i sintel.mpg -pass 1 -t 75 -c:v libx264 -an nul.mp4"
 
