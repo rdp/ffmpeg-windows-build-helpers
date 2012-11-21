8e584485ca7360a0c1b8b0f697c920ac8c61b481
rogerdpack <rogerpack2005@gmail.com>
2012-11-21 15:05:45 -0700

    strip them on way out, too, for dll's

--- collect.sh
+++ collect.sh
@@ -12,9 +12,11 @@ mkdir $dir/32
 #mkdir $dir/64
 cp ./sandbox/win32/ffmpeg_git/ffmpeg.exe "$dir/32/ffmpeg.exe"
 cp ./sandbox/win32/ffmpeg_git/*/*.dll     "$dir/32"
+./sandbox/mingw-w64-i686/bin/i686-w64-mingw32-strip $dir/32/*.dll
 #cp ./sandbox/win32/ffmpeg_git/ffplay.exe "$dir/32/ffplay.exe"
 #cp ./sandbox/win32/ffmpeg_git/ffmpeg_g.exe "$dir/32/ffmpeg_g.exe"
 #cp ./sandbox/x86_64/ffmpeg_git/ffmpeg.exe "$dir/64/ffmpeg.exe"
+#cp ./sandbox/win32/ffmpeg_git/*/*.dll     "$dir/64"
 #cp ./sandbox/x86_64/ffmpeg_git/ffmpeg_g.exe "$dir/64/ffmpeg_g.exe"
 cd distros
 7zr a "$file.7z" "$file/*"
