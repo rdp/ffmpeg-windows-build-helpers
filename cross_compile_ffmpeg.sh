dc72017468a86933f1ba5332ce03db8e2093f99b
rogerdpack <rogerpack2005@gmail.com>
2012-11-21 10:26:31 -0700

    fix rtmp pkg-config

--- cross_compile_ffmpeg.sh
+++ cross_compile_ffmpeg.sh
@@ -202,6 +202,7 @@ build_librtmp() {
   cd rtmpdump_git/librtmp
   git co 883c33489403ed360a01d1a47ec76d476525b49e # trunk didn't build once...
   make install CRYPTO=GNUTLS OPT='-O2 -g' "CROSS_COMPILE=$cross_prefix" SHARED=no "prefix=$mingw_w64_x86_64_prefix" || exit 1
+  sed -i 's/-lrtmp -lz/-lrtmp -lwinmm -lz/' "$PKG_CONFIG_PATH/librtmp.pc"
   cd ../..
 }
 
