69210eea96c59aa97b2464dd053fa5eb0d85d11b
rogerdpack <rogerpack2005@gmail.com>
2013-02-15 20:05:52 -0700

    note packaging is broken

--- collect.sh
+++ collect.sh
@@ -15,17 +15,23 @@ if [ -f ./sandbox/x86_64/ffmpeg_git/ffmpeg.exe ]; then
   mkdir $dir/64-bit
 fi
 
+# TODO include shared builds too, now, also maybe separate zips for everything, put them in their own folder?
+
 cp ./sandbox/win32/ffmpeg_git/ffmpeg.exe "$dir/32-bit"
 #cp ./sandbox/win32/ffmpeg_git/ffplay.exe "$dir/32-bit"
-#cp ./sandbox/win32/ffmpeg_git/ffmpeg_g.exe "$dir/32-bit"
-cp ./sandbox/win32/ffmpeg_git/*/*-*.dll     "$dir/32-bit"
-./sandbox/mingw-w64-i686/bin/i686-w64-mingw32-strip $dir/32-bit/*.dll
+cp ./sandbox/win32/ffmpeg_git/ffmpeg_g.exe "$dir/32-bit"
 
-cp ./sandbox/x86_64/ffmpeg_git/ffmpeg.exe "$dir/64-bit"
+# old shared build stuff...
+#cp ./sandbox/win32/ffmpeg_git/*/*-*.dll     "$dir/32-bit" 
+#./sandbox/mingw-w64-i686/bin/i686-w64-mingw32-strip $dir/32-bit/*.dll
+
+#cp ./sandbox/x86_64/ffmpeg_git/ffmpeg.exe "$dir/64-bit"
 #cp ./sandbox/x86_64/ffmpeg_git/ffplay.exe "$dir/64-bit"
 #cp ./sandbox/x86_64/ffmpeg_git/ffmpeg_g.exe "$dir/64-bit"
-cp ./sandbox/x86_64/ffmpeg_git/*/*-*.dll     "$dir/64-bit"
-./sandbox/mingw-w64-x86_64/bin/x86_64-w64-mingw32-strip $dir/64-bit/*.dll
+
+# old broken shared stuff...
+#cp ./sandbox/x86_64/ffmpeg_git/*/*-*.dll     "$dir/64-bit"
+#./sandbox/mingw-w64-x86_64/bin/x86_64-w64-mingw32-strip $dir/64-bit/*.dll
 
 cd distros
 7zr a "$file.7z" "$file/*"
