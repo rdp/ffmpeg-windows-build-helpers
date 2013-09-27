dd3d2c2049a3b579e27b4dc4bb563b47c7a20d79
rogerdpack <rogerpack2005@gmail.com>
2013-09-27 17:53:30 -0600

    simplify collect script, add shared always

--- collect.sh
+++ collect.sh
@@ -1,12 +1,18 @@
+set -x
+
 cd sandbox/win32/ffmpeg_git
 git_version=`git rev-parse HEAD`
-echo $git_version
+echo "creating distro for $git_version"
 cd ../../..
-mkdir -p distros
-date=`date +%Y-%m-%d`
-file="ffmpeg-distro-static-$date-$git_version"
-dir="distros/$file"
-rm -rf $dir
+mkdir -p distros # -p so it doesn't warn
+date=`date +%Y-%m-%d-%H%M`
+
+file="distro-$date"
+root="distros/$file"
+rm -rf $root
+mkdir -p $root
+
+dir="$root/$file-ffmpeg-static"
 mkdir $dir
 if [ -f ./sandbox/win32/ffmpeg_git/ffmpeg.exe ]; then
   mkdir $dir/32-bit
@@ -15,26 +21,36 @@ if [ -f ./sandbox/x86_64/ffmpeg_git/ffmpeg.exe ]; then
   mkdir $dir/64-bit
 fi
 
-# TODO include shared builds too, now, also maybe separate zips for everything, put them in their own folder?
-
 cp ./sandbox/win32/ffmpeg_git/ffmpeg.exe "$dir/32-bit"
-#cp ./sandbox/win32/ffmpeg_git/ffplay.exe "$dir/32-bit"
-#cp ./sandbox/win32/ffmpeg_git/ffmpeg_g.exe "$dir/32-bit"
+cp ./sandbox/win32/ffmpeg_git/ffplay.exe "$dir/32-bit"
+cp ./sandbox/win32/ffmpeg_git/ffmpeg_g.exe "$dir/32-bit"
 
-# old shared build stuff...
-#cp ./sandbox/win32/ffmpeg_git/*/*-*.dll     "$dir/32-bit" 
-#./sandbox/mingw-w64-i686/bin/i686-w64-mingw32-strip $dir/32-bit/*.dll
+cp ./sandbox/x86_64/ffmpeg_git/ffmpeg.exe "$dir/64-bit"
+cp ./sandbox/x86_64/ffmpeg_git/ffplay.exe "$dir/64-bit"
+cp ./sandbox/x86_64/ffmpeg_git/ffmpeg_g.exe "$dir/64-bit"
 
-#cp ./sandbox/x86_64/ffmpeg_git/ffmpeg.exe "$dir/64-bit"
-#cp ./sandbox/x86_64/ffmpeg_git/ffplay.exe "$dir/64-bit"
-#cp ./sandbox/x86_64/ffmpeg_git/ffmpeg_g.exe "$dir/64-bit"
+dir="$root/$file-ffmpeg-shared"
+mkdir $dir
+mkdir $dir/32-bit
+mkdir $dir/64-bit
 
-# old broken shared stuff...
-#cp ./sandbox/x86_64/ffmpeg_git/*/*-*.dll     "$dir/64-bit"
-#./sandbox/mingw-w64-x86_64/bin/x86_64-w64-mingw32-strip $dir/64-bit/*.dll
+cp ./sandbox/win32/ffmpeg_git_shared/ffmpeg.exe "$dir/32-bit"
+cp ./sandbox/win32/ffmpeg_git_shared/ffplay.exe "$dir/32-bit"
+cp ./sandbox/win32/ffmpeg_git_shared/ffmpeg_g.exe "$dir/32-bit"
+
+cp ./sandbox/win32/ffmpeg_git_shared/*/*-*.dll     "$dir/32-bit"  # have to flatten it
+./sandbox/mingw-w64-i686/bin/i686-w64-mingw32-strip $dir/32-bit/*.dll # XXX debug dll's?
+
+cp ./sandbox/x86_64/ffmpeg_git_shared/ffmpeg.exe "$dir/64-bit"
+cp ./sandbox/x86_64/ffmpeg_git_shared/ffplay.exe "$dir/64-bit"
+cp ./sandbox/x86_64/ffmpeg_git_shared/ffmpeg_g.exe "$dir/64-bit"
+
+cp ./sandbox/x86_64/ffmpeg_git_shared/*/*-*.dll     "$dir/64-bit"  # have to flatten it
+./sandbox/mingw-w64-x86_64/bin/x86_64-w64-mingw32-strip $dir/64-bit/*.dll # XXX debug dll's?
 
 cd distros
-7zr a "$file.7z" "$file/*" || 7za a "$file.7z" "$file/*"  # some have a package with only 7za, see https://github.com/rdp/ffmpeg-windows-build-helpers/issues/16
+# -mx=1 fastest compression speed [but biggest file]
+7zr -mx=1 a "$file.7z" "$file/*" || 7za a "$file.7z" "$file/*"  # some have a package with only 7za, see https://github.com/rdp/ffmpeg-windows-build-helpers/issues/16
 cd ..
 
-echo "created distros/$file"
+echo "created distros/$file.7z"
