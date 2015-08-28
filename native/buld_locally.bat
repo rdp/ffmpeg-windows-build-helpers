395e90e9a747039031e6e4ff6131bae296ecc28f
rogerdpack <rogerpack2005@gmail.com>
2015-08-28 14:21:33 -0600

    combie batch fies, hope it works

--- native/buld_locally.bat
+++ native/buld_locally.bat
@@ -24,3 +24,20 @@ echo "done installing cygwin"
 @rem TODO run the script LOL
 
 cd .. 
+
+setlocal
+@rem want wget etc. so override path. Probably need this anyway...
+set PATH=%cd%\cygwin_install\bin;%PATH%
+
+mkdir ffmpeg_build
+cd ffmpeg_build
+
+..\cygwin_install\bin\bash.exe -c "wget https://raw.github.com/rdp/ffmpeg-windows-build-helpers/master/cross_compile_ffmpeg.sh -O cross_compile_ffmpeg.sh"
+..\cygwin_install\bin\bash.exe -c "chmod u+x ./cross_compile_ffmpeg.sh"
+..\cygwin_install\bin\bash.exe -c "./cross_compile_ffmpeg.sh"
+
+cd ..
+
+ECHO "done with local build..."
+
+pause
