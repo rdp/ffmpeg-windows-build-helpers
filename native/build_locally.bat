217bf6c301f607b7eeeb0c666e60e6b144f40b79
rogerdpack <rogerpack2005@gmail.com>
2015-08-28 11:27:28 -0600

    does auto install now

--- native/build_locally.bat
+++ native/build_locally.bat
@@ -2,16 +2,27 @@ ECHO downloading cygwin setup exe...
 wscript download.vbs
 
 ECHO running cygwin install
+mkdir %cd%\cygwin_install
+@rem cd to it so that cygwin install logs go there
+cd %cd%\cygwin_install
 
 setup-x86.exe ^
 --quiet-mode ^
 --no-admin ^
 --no-startmenu ^
+--no-shortcuts ^
 --no-desktop ^
---root .\cygwin_root ^
+--site http://mirrors.xmission.com/cygwin/ ^
+--root %cd% ^
 --packages ^
 curl,^
 atool,^
 autoconf,^
 automake,^
 autossh
+
+@rem TODO more
+
+echo "done installing cygwin"
+
+@rem TODO run the script LOL
