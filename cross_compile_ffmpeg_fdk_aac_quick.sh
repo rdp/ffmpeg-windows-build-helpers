574eaa1656232c5bf08db35179c1e658b1b0b620
rogerdpack <rogerpack2005@gmail.com>
2015-09-02 14:58:09 -0600

    if we're going to do an install, may as well put it in its own folder and advertise the same

--- cross_compile_ffmpeg_fdk_aac_quick.sh
+++ cross_compile_ffmpeg_fdk_aac_quick.sh
@@ -65,9 +65,9 @@ cd ffmpeg
      echo $PKG_CONFIG_PATH "was pkg config path"
     ./configure --enable-gpl --enable-libx264 --enable-nonfree \
       --enable-libfdk-aac --arch=x86 --target-os=mingw32 \
-      --cross-prefix=$host- --pkg-config=pkg-config
+      --cross-prefix=$host- --pkg-config=pkg-config --prefix=$(prefix)/ffmpeg_static
   fi
   rm **/*.a # attempt force a rebuild...
-  make -j5 install && echo "created ffmpeg.exe in $(pwd)/ffmpeg.exe!"
+  make -j5 install && echo "created runnable ffmpeg.exe in $(prefix)/ffmpeg_static/ffmpeg.exe!"
   
 cd ..
