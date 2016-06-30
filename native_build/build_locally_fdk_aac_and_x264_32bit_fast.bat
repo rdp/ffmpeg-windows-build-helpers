e4952fb73bff28003cb9fee70d10742deaeeb099
rogerdpack <rogerpack2005@gmail.com>
2016-06-29 20:15:43 -0600

    rename fast

--- native_build/build_locally_fdk_aac_and_x264_32bit_fast.bat
+++ native_build/build_locally_fdk_aac_and_x264_32bit_fast.bat
@@ -4,9 +4,9 @@ ECHO Starting cygwin install/update...
 
 @rem use the same dir so it can be reused cygwin install :)
 
-mkdir ffmpeg_local_builds\cygwin_local_install
+mkdir ffmpeg_local_builds\cygwin_local_install_fast
 @rem cd to it so that cygwin install logs etc. go there
-cd ffmpeg_local_builds\cygwin_local_install
+cd ffmpeg_local_builds\cygwin_local_install_fast
 ECHO Downloading local cygwin setup executable...some error logs are expected...
 @powershell -command "$clnt = new-object System.Net.WebClient; $clnt.DownloadFile(\"https://cygwin.com/setup-x86_64.exe\", \"setup-x86_64.exe\")"
 
@@ -28,13 +28,13 @@ echo "done installing cygwin"
 cd ..\..
 
 setlocal
-set PATH=%cd%\ffmpeg_local_builds\cygwin_local_install\bin;%PATH%
+set PATH=%cd%\ffmpeg_local_builds\cygwin_local_install_fast\bin;%PATH%
 
 cd ffmpeg_local_builds
 
-.\cygwin_local_install\bin\bash.exe -c "wget https://raw.github.com/rdp/ffmpeg-windows-build-helpers/master/quick_cross_compile_ffmpeg_fdk_aac_and_x264_using_packaged_mingw64.sh -O quick_cross_compile_ffmpeg_fdk_aac_and_x264_using_packaged_mingw64.sh"
-.\cygwin_local_install\bin\bash.exe -c "chmod u+x ./quick_cross_compile_ffmpeg_fdk_aac_and_x264_using_packaged_mingw64.sh"
-.\cygwin_local_install\bin\bash.exe -c ./quick_cross_compile_ffmpeg_fdk_aac_and_x264_using_packaged_mingw64.sh
+.\cygwin_local_install_fast\bin\bash.exe -c "wget https://raw.github.com/rdp/ffmpeg-windows-build-helpers/master/quick_cross_compile_ffmpeg_fdk_aac_and_x264_using_packaged_mingw64.sh -O quick_cross_compile_ffmpeg_fdk_aac_and_x264_using_packaged_mingw64.sh"
+.\cygwin_local_install_fast\bin\bash.exe -c "chmod u+x ./quick_cross_compile_ffmpeg_fdk_aac_and_x264_using_packaged_mingw64.sh"
+.\cygwin_local_install_fast\bin\bash.exe -c ./quick_cross_compile_ffmpeg_fdk_aac_and_x264_using_packaged_mingw64.sh
 
 ECHO done with local build...check output above to see if successfull...
 pause
