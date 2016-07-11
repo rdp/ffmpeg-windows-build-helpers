656e60e971423f06a559cf291a9a5a50d062d26e
rogerdpack <rogerpack2005@gmail.com>
2016-07-11 17:40:36 -0600

    avoid differing git versions from the same build :|

--- patches/all_build.sh
+++ patches/all_build.sh
@@ -1,5 +1,14 @@
-./cross_compile_ffmpeg.sh --compiler-flavors=multi --disable-nonfree=y --git-get-latest=y
-./cross_compile_ffmpeg.sh --compiler-flavors=multi --disable-nonfree=y --git-get-latest=y --build-intel-qsv=n # windows xp
-./cross_compile_ffmpeg.sh --compiler-flavors=multi --disable-nonfree=y --high-bitdepth=y --git-get-latest=y
+for dir in sandbox/*/ffmpeg_git*; do
+  cd $dir
+  git pull
+  rm already*
+  cd ../../.. 
+done
+
+./cross_compile_ffmpeg.sh --compiler-flavors=multi --disable-nonfree=y --git-get-latest=n # static
+./cross_compile_ffmpeg.sh --compiler-flavors=multi --disable-nonfree=y --git-get-latest=n --build-intel-qsv=n # windows xp
+./cross_compile_ffmpeg.sh --compiler-flavors=multi --disable-nonfree=y --high-bitdepth=n --git-get-latest=y # high bit depth
+
 ./patches/collect.sh
+
 echo "now upload them!"
