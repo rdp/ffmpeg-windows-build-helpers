b34d358b44421cbf3de093a279c9536992727d75
rogerdpack <rogerpack2005@gmail.com>
2016-08-06 11:54:34 -0600

    and actually build high bitdepth LOL

--- patches/all_build.sh
+++ patches/all_build.sh
@@ -5,15 +5,18 @@ rm -rf sandbox/distros # free up space
 # so that packaging doesn't detect discrepancies and barf :)
 for dir in sandbox/*/ffmpeg_git*; do
   cd $dir
-  git pull
-  rm already*
+  if [[ $1 == "" ]]; then
+    git pull
+  fi 
+  # else don't do git pull as it resets the git hash so forces a rebuild even if it's already previously built to that hash, ex: release build
+  rm -f already*
   cd ../../.. 
 done
 
 # all are both 32 and 64 bit
-./cross_compile_ffmpeg.sh --compiler-flavors=multi --disable-nonfree=y --git-get-latest=n --build-ffmpeg-shared=y $1 # normal static and shared
-./cross_compile_ffmpeg.sh --compiler-flavors=multi --disable-nonfree=y --git-get-latest=n --build-intel-qsv=n $1 # windows xp static
-./cross_compile_ffmpeg.sh --compiler-flavors=multi --disable-nonfree=y --high-bitdepth=n --git-get-latest=y $1 # high bit depth static
+./cross_compile_ffmpeg.sh --compiler-flavors=multi --disable-nonfree=y --git-get-latest=n --build-ffmpeg-shared=y $1 && # normal static and shared
+./cross_compile_ffmpeg.sh --compiler-flavors=multi --disable-nonfree=y --git-get-latest=n --build-intel-qsv=n $1 && # windows xp static
+./cross_compile_ffmpeg.sh --compiler-flavors=multi --disable-nonfree=y --git-get-latest=y --high-bitdepth=y $1 # high bit depth static
 
 ./patches/collect.sh
 touch ./sandbox/distros/readme.txt
