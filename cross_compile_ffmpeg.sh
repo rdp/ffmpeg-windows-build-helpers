3992737fb54136744422c1989208c25a180316b9
rogerdpack <rogerpack2005@gmail.com>
2017-07-18 13:55:27 -0600

    use an http git for those of us behind draconian firewalls and can't access it

--- cross_compile_ffmpeg.sh
+++ cross_compile_ffmpeg.sh
@@ -960,10 +960,11 @@ build_libbluray() {
   do_git_checkout https://git.videolan.org/git/libbluray.git
   cd libbluray_git
     if [[ ! -d .git/modules ]]; then
+      sed -i.bak 's_git://git.videolan.org/libudfread.git_https://git.videolan.org/git/libudfread.git_' .gitmodules
       git submodule update --init --remote # For UDF support [default=enabled], which strangely enough is in another repository.
     else
       local local_git_version=`git --git-dir=.git/modules/contrib/libudfread rev-parse HEAD`
-      local remote_git_version=`git ls-remote -h git://git.videolan.org/libudfread.git | sed "s/\s.*//"`
+      local remote_git_version=`git ls-remote -h https://git.videolan.org/git/libudfread.git | sed "s/\s.*//"`
       if [[ "$local_git_version" != "$remote_git_version" ]]; then
         git clean -f # Throw away local changes; 'already_*' in this case.
         git submodule foreach -q 'git clean -f' # Throw away local changes; 'already_configured_*' and 'udfread.c.bak' in this case.
