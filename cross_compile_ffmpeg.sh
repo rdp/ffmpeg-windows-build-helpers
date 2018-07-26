c4cbe3e0510af121a46a42478d2d7524c0f3e2c9
rogerdpack <rogerpack2005@gmail.com>
2018-07-25 19:23:11 -0600

    message too late plus odd

--- cross_compile_ffmpeg.sh
+++ cross_compile_ffmpeg.sh
@@ -2189,8 +2189,6 @@ if [[ $compiler_flavors == "multi" || $compiler_flavors == "win64" ]]; then
     build_ffmpeg_dependencies
     build_apps
   cd ..
-else
-  echo "doing local native build for your current OS!"
 fi
 
 echo "searching for all local exe's (some may not have been built this round, NB)..."
