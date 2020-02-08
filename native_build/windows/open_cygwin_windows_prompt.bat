6009d92771555b1b444baa0880b9e682155c66e0
rogerdpack <rogerpack2005@gmail.com>
2020-02-08 08:27:38 -0700

    remove support for cygwin [again] see earlier commit

--- native_build/windows/open_cygwin_windows_prompt.bat
+++
@@ -1,5 +0,0 @@
-@setlocal
-@ECHO cd to 
-@.\ffmpeg_local_builds\cygwin_local_install\bin\cygpath.exe -a ffmpeg_local_builds
-@echo "for the root ffmpeg build dir"
-@ffmpeg_local_builds\cygwin_local_install\bin\bash.exe --login -i
