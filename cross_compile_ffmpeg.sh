24c074f8f7321fa3a2fb71b377f956a71242ba52
Emmanuel Mathi-Amorim <manny@amorim.ca>
2021-02-13 18:28:56 -0500

    fix branch name for librubberband

--- cross_compile_ffmpeg.sh
+++ cross_compile_ffmpeg.sh
@@ -1546,7 +1546,7 @@ build_libsamplerate() {
 }
 
 build_librubberband() {
-  do_git_checkout https://github.com/breakfastquay/rubberband.git
+  do_git_checkout https://github.com/breakfastquay/rubberband.git rubberband_git default
   cd rubberband_git
     apply_patch file://$patch_dir/rubberband_git_static-lib.diff # create install-static target
     do_configure "--host=$host_target --prefix=$mingw_w64_x86_64_prefix --disable-ladspa"
