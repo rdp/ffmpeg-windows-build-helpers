6f940caf90f09924e3c70cfa131697be14e161c8
Roger Pack <rogerpack2005@gmail.com>
2022-02-12 06:29:37 +0000

    bump lame #606

--- cross_compile_ffmpeg.sh
+++ cross_compile_ffmpeg.sh
@@ -1339,10 +1339,10 @@ build_libsndfile() {
 }
 
 build_lame() {
-  do_svn_checkout https://svn.code.sf.net/p/lame/svn/trunk/lame lame_svn r6474
+  do_svn_checkout https://svn.code.sf.net/p/lame/svn/trunk/lame lame_svn 
   cd lame_svn
     sed -i.bak '1s/^\xEF\xBB\xBF//' libmp3lame/i386/nasm.h # Remove a UTF-8 BOM that breaks nasm if it's still there; should be fixed in trunk eventually https://sourceforge.net/p/lame/patches/81/
-    generic_configure "--enable-nasm"
+    generic_configure "--enable-nasm --disable-decoder"
     do_make_and_make_install
   cd ..
 }
