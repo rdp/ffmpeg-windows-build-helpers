ff31946970db9d8dc6166db1badec90d7ebe40df
Roger Pack <rogerpack2005@gmail.com>
2022-04-22 03:32:43 +0000

    svt option

--- cross_compile_ffmpeg.sh
+++ cross_compile_ffmpeg.sh
@@ -2623,7 +2623,7 @@ build_ffmpeg_dependencies() {
   build_libsamplerate # Needs libsndfile >= 1.0.6 and fftw >= 0.15.0 for tests. Uses dlfcn.
   build_librubberband # Needs libsamplerate, libsndfile, fftw and vamp_plugin. 'configure' will fail otherwise. Eventhough librubberband doesn't necessarily need them (libsndfile only for 'rubberband.exe' and vamp_plugin only for "Vamp audio analysis plugin"). How to use the bundled libraries '-DUSE_SPEEX' and '-DUSE_KISSFFT'?
   build_frei0r # Needs dlfcn. could use opencv...
-  if [ "$bits_target" != "32" ]; then
+  if [ "$bits_target" != "32" && $build_svt = y]; then
     build_svt-hevc
     build_svt-av1
     build_svt-vp9
